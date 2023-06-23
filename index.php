753e81df54df5aa6f7f4a30508b22e5bcfdd3695
ALI736-WDS <ali.sh.p736@gmail.com>
2023-06-23 16:17:35 +0330

    increase tag audio and changes in style in index.php

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ include 'WDS/includes/menu main site.php';
 
 
 						<p><a href="WDS/blog/links.php?id=<?= $show_text_link['id'] ?>" class="pl-5 link-download-p-a-pl-xl" target="_blank">
-								<svg class="svg-seen mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
+								<svg class="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
 									<path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
 									<path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
 								</svg>
@@ -151,7 +151,27 @@ include 'WDS/includes/menu main site.php';
 		<p><a href="http://shafayehelahi2.ir//WDS/download/ketabeh darmaneh bimariha jesmi va ruhi ravani va gheire.pdf.zip" target="_blank" > لینک دانلود PDF کتاب بصورت رایگان</a></p>	
 -->
 			<!--<p><a href="https://idpay.ir/mojtaba-khansari/file/163304" target="_blank" > لینک دانلود فایل  PDF کتاب </a></p>-->
-			<!--	<p><a href="https://idpay.ir/mojtaba-khansari/file/163301" target="_blank" > لینک دانلود فایل PDF دستورالعمل ضمیمه</a></p>-->
+			<!--<p><a href="https://idpay.ir/mojtaba-khansari/file/163301" target="_blank" > لینک دانلود فایل PDF دستورالعمل ضمیمه</a></p>-->
+
+			<!--//////////////////////////////////////////////////////////////////////////////-->
+			<!--//////////////////////////////////////////////////////////////////////////////-->
+			<!--//////////////////////////////////////////////////////////////////////////////-->
+			<!--//////////////////////////////////////////////////////////////////////////////-->
+
+			<div class="audio">
+				<audio controls>
+					<source src="./WDS/download/audio/4_5803157092193799764.mp3" type="audio/mpeg">
+				</audio>
+				<audio controls>
+					<source src="./WDS/download/audio/4_5803157092193799773.mp3" type="audio/mpeg">
+				</audio>
+				<audio controls>
+					<source src="./WDS/download/audio/الرقية الشرعية للشيخ خالد الحبشي آيات حرق الجني_.mp3" type="audio/mpeg">
+				</audio>
+				<audio controls>
+					<source src="./WDS/download/audio/آيات الحرق والعذاب للجنى المستعصى.mp3" type="audio/mpeg">
+				</audio>
+			</div>
 
 			<!--//////////////////////////////////////////////////////////////////////////////-->
 			<!--//////////////////////////////////////////////////////////////////////////////-->
